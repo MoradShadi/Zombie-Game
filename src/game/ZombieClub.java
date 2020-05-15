64c8f8a86cea3c1da43a213ee133a6f4f69110a2
Morad <mabo0003@student.monash.edu>
2020-05-15 17:14:04 +0800

    Zombie club class

---
+++ src/game/ZombieClub.java
@@ -0,0 +1,12 @@
+package game;
+
+import edu.monash.fit2099.engine.WeaponItem;
+
+public class ZombieClub extends WeaponItem{
+
+	public ZombieClub() {
+	super("Zombie Club", ';', 20, "nails");
+		// TODO Auto-generated constructor stub
+	}
+
+}
