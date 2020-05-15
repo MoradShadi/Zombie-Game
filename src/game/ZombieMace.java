8b3835bfca22fc0b88149d5176f6e6b97fc879c5
Morad <mabo0003@student.monash.edu>
2020-05-15 17:01:24 +0800

    Zombie Mace class

---
+++ src/game/ZombieMace.java
@@ -0,0 +1,12 @@
+package game;
+
+import edu.monash.fit2099.engine.WeaponItem;
+
+public class ZombieMace extends WeaponItem{
+
+	public ZombieMace() {
+		super("Zombie Mace", '/', 25, "strikes");
+		// TODO Auto-generated constructor stub
+	}
+
+}
