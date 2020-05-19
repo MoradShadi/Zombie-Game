8e1975722b526dc14b7224f97d3256c3f7c38663
Morad <mabo0003@student.monash.edu>
2020-05-19 17:49:25 +0800

    updated pickupweapon functionality
    drop any weapon before picking up new on

--- src/game/PickUpWeaponBehaviour.java
+++ src/game/PickUpWeaponBehaviour.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import edu.monash.fit2099.engine.Action;
 import edu.monash.fit2099.engine.Actor;
+import edu.monash.fit2099.engine.DropItemAction;
 import edu.monash.fit2099.engine.GameMap;
 import edu.monash.fit2099.engine.Item;
 import edu.monash.fit2099.engine.PickUpItemAction;
@@ -17,8 +18,14 @@ public class PickUpWeaponBehaviour implements Behaviour {
 	@Override
 	public Action getAction(Actor actor, GameMap map) {
 		List<Item> items = new ArrayList<Item>(map.locationOf(actor).getItems());
+		List<Item> inventory = new ArrayList<Item>(actor.getInventory());
+
 		for (Item item: items) {
 			if (item instanceof WeaponItem) 
+				for(Item item2: inventory) {
+					if(item2 instanceof WeaponItem)
+						  new DropItemAction(item2).execute(actor, map);
+				}
 				return new PickUpItemAction(item);
 		}
 		return null;
