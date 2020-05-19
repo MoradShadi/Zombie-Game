748873ab33b6d4e74a930e530718bda57ac0545a
Morad <mabo0003@student.monash.edu>
2020-05-19 13:51:50 +0800

    Added functionality to player to craft weapon

--- src/game/Player.java
+++ src/game/Player.java
@@ -1,11 +1,13 @@
 package game;
 
+import java.util.List;
 import java.util.Random;
 
 import edu.monash.fit2099.engine.Action;
 import edu.monash.fit2099.engine.Actions;
 import edu.monash.fit2099.engine.Display;
 import edu.monash.fit2099.engine.GameMap;
+import edu.monash.fit2099.engine.Item;
 import edu.monash.fit2099.engine.Menu;
 import edu.monash.fit2099.engine.Weapon;
 
@@ -49,6 +51,11 @@ public class Player extends Human {
 	@Override
 	public Action playTurn(Actions actions, Action lastAction, GameMap map, Display display) {
 		// Handle multi-turn Actions
+		List<Item> items = this.getInventory();
+		for (Item item: items) {
+			if (item instanceof ZombieLeg || item instanceof ZombieArm)
+				actions.add(new CraftWeaponAction(item));
+		}
 		if (lastAction.getNextAction() != null)
 			return lastAction.getNextAction();
 		return menu.showMenu(this, actions, display);
