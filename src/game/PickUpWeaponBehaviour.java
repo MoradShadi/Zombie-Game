bc1e15e97c0fc9871e4ea55a62896545372dd4b2
Morad <mabo0003@student.monash.edu>
2020-05-19 14:46:38 +0800

    PickUpWeaponBehaviour functionality implemented

--- src/game/PickUpWeaponBehaviour.java
+++ src/game/PickUpWeaponBehaviour.java
@@ -1,13 +1,26 @@
 package game;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import edu.monash.fit2099.engine.Action;
 import edu.monash.fit2099.engine.Actor;
 import edu.monash.fit2099.engine.GameMap;
+import edu.monash.fit2099.engine.Item;
+import edu.monash.fit2099.engine.PickUpItemAction;
+import edu.monash.fit2099.engine.WeaponItem;
+
 
 public class PickUpWeaponBehaviour implements Behaviour {
 
+	
 	@Override
 	public Action getAction(Actor actor, GameMap map) {
-		
+		List<Item> items = new ArrayList<Item>(map.locationOf(actor).getItems());
+		for (Item item: items) {
+			if (item instanceof WeaponItem) 
+				return new PickUpItemAction(item);
+		}
+		return null;
 	}
 }
