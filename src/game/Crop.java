fc02b6002977b0d724138bb124c53d613dabd081
Nur Farhanah <nbah0001@student.monash.edu>
2020-05-20 09:19:48 +0800

    added methods to ripen crop

--- src/game/Crop.java
+++ src/game/Crop.java
@@ -6,8 +6,25 @@ import edu.monash.fit2099.engine.Ground;
  * A class that represents bare dirt.
  */
 public class Crop extends Ground {
-
-	public Crop() {
+	int turn;
+	boolean riped;
+	
+	public Crop(int turns) {
 		super('^');
+		this.turn = turns;
+	}
+	
+	public void boostRipen() {
+		turn -= 10;
+		if (turn <= 0) {
+			riped = true;
+		}
+	}
+	
+	public void normalRipen() {
+		turn -= 1;
+		if (turn <= 0) {
+			riped = true;
+		}
 	}
 }
