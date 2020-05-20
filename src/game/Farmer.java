815313605e8b656aca9053acf284b370424127bc
Nur Farhanah <nbah0001@student.monash.edu>
2020-05-20 14:41:39 +0800

    fixed sowChance() and added fertilizeCrop()

--- src/game/Farmer.java
+++ src/game/Farmer.java
@@ -3,6 +3,7 @@ package game;
 import java.util.Random;
 
 import edu.monash.fit2099.engine.GameMap;
+import edu.monash.fit2099.engine.Ground;
 import edu.monash.fit2099.engine.Location;
 
 /**
@@ -10,8 +11,6 @@ import edu.monash.fit2099.engine.Location;
  */
 public class Farmer extends Human {
 	private Random rand = new Random();
-	private double sowingChance = rand.nextDouble();
-	private Crop crop;
 	private Location[][] map;
 
 	/**
@@ -26,9 +25,43 @@ public class Farmer extends Human {
 	}
 	
 	public void sowChance(int x, int y, GameMap gameMap) {
-		if(map[x][y].equals(crop.getLocation()))
-			if (sowingChance <= 0.33) {
-				new SowCrop(x, y, gameMap);
+		// location for left
+		int x_left = x - 1;
+		int x_right = x + 1;
+			
+		Ground ground_l = map[x_left][y].getGround();
+		Ground ground_r = map[x_right][y].getGround();
+			
+		if(ground_l.getDisplayChar() == '.') {
+			double sowChance = rand.nextDouble();
+
+			if (sowChance <= 0.33) {
+				new SowCrop(x_left, y, gameMap);
 			}
+		}
+		
+		if(ground_r.getDisplayChar() == '.') {
+			double sowChance = rand.nextDouble();
+
+			if (sowChance <= 0.33) {
+				new SowCrop(x_left, y, gameMap);
+			}	
+		}
+	}
+	
+	public void fertilizeCrop(int x, int y, GameMap gameMap) {
+		Ground ground = map[x][y].getGround();
+		if(ground.getDisplayChar() == '^') {
+			Crop crop = (Crop) ground;
+			if(crop.isRipe() == false) {
+				crop.boostRipen();
+			}
+		}
+		
+		double sowingChance = rand.nextDouble();
+		
+		if (sowingChance <= 0.33) {
+			new SowCrop(x, y, gameMap);
+		}	
 	}
 }
