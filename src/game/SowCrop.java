24cea6c2ee6bc919c50aaf665c1ac276bdbd8718
Nur Farhanah <nbah0001@student.monash.edu>
2020-05-21 16:20:20 +0800

    added string output to be displayed to player and fixed functionality to avoid errors

--- src/game/SowCrop.java
+++ src/game/SowCrop.java
@@ -1,5 +1,6 @@
 package game;
 
+import java.util.ArrayList;
 import java.util.Random;
 import edu.monash.fit2099.engine.Action;
 import edu.monash.fit2099.engine.Actor;
@@ -8,42 +9,50 @@ import edu.monash.fit2099.engine.Ground;
 import edu.monash.fit2099.engine.Location;
 
 public class SowCrop implements Behaviour {
+	ArrayList<Crop> crops = new ArrayList<>();
 	Location location;
 	Location [][] map;
 	private Random rand = new Random();
 	
 	public void sow(int x, int y, GameMap gameMap) {
 		Crop crop = new Crop(20, location);
+		crops.add(crop);
 		gameMap.at(x, y).setGround(crop);
+		System.out.println("A farmer has sowed a crop.");
 	}
 
 	@Override
 	public Action getAction(Actor actor, GameMap gameMap) {
 		// TODO Auto-generated method stub
-		location = gameMap.locationOf(actor);
+		Farmer farmer = (Farmer) actor;
+		location = gameMap.locationOf(farmer);
 		int x = location.x();
 		int y = location.y();
 		int x_left = x - 1;
 		int x_right = x + 1;
-		System.out.println(x);
+		
+		if(x_left >= 0) {
+			Ground ground_l = gameMap.at(x_left, y).getGround();
 			
-		Ground ground_l = map[x_left][y].getGround();
-		Ground ground_r = map[x_right][y].getGround();
-			
-		if(ground_l.getDisplayChar() == '.') {
-			double sowChance = rand.nextDouble();
+			if(ground_l.getDisplayChar() == '.') {
+				double sowChance = rand.nextDouble();
 
-			if (sowChance <= 0.33) {
-				this.sow(x_left, y, gameMap);
+				if (sowChance <= 0.33) {
+					this.sow(x_left, y, gameMap);
+				}
 			}
 		}
 		
-		if(ground_r.getDisplayChar() == '.') {
-			double sowChance = rand.nextDouble();
+		if(x_right >= 0) {
+			Ground ground_r = gameMap.at(x_right, y).getGround();
+			
+			if(ground_r.getDisplayChar() == '.') {
+				double sowChance = rand.nextDouble();
 
-			if (sowChance <= 0.33) {
-				sow(x_left, y, gameMap);
-			}	
+				if (sowChance <= 0.33) {
+					sow(x_right, y, gameMap);
+				}	
+			}
 		}
 		return null;
 	}
