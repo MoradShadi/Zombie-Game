e4a8057fd40ed7d428c614b81b871c092e3bad91
Khairenn <klai0011@student.monash.edu>
2020-05-22 20:18:49 +0800

    Eating food javadoc

--- src/game/EatFoodAction.java
+++ src/game/EatFoodAction.java
@@ -4,21 +4,37 @@ import edu.monash.fit2099.engine.Action;
 import edu.monash.fit2099.engine.Actor;
 import edu.monash.fit2099.engine.GameMap;
 
+/**
+ * Special action for eating food
+ * 
+ * @author User
+ *
+ */
 public class EatFoodAction extends Action {
-	private int hpRestored;
 	private Food foodToRemove;
 
+	/**
+	 * Constructor
+	 * 
+	 * @param initFood the food that will be eaten
+	 */
 	public EatFoodAction(Food initFood) {
 		// TODO Auto-generated constructor stub
 		foodToRemove = initFood;
-		hpRestored = 10;
 	}
 
+	/**
+	 * Perform the eating action and heal the actor for the Food heal amount
+	 * 
+	 * @param actor The actor that is eating.
+	 * @param map The map the actor is on.
+	 * @return a description of what happened that can be displayed to the user.
+	 */
 	@Override
 	public String execute(Actor actor, GameMap map) {
 		// TODO Auto-generated method stub
+		actor.heal(foodToRemove.getHealAmount());
 		actor.removeItemFromInventory(foodToRemove);
-		actor.heal(hpRestored);
 		return menuDescription(actor);
 	}
 
--- src/game/EatFoodBehaviour.java
+++ src/game/EatFoodBehaviour.java
@@ -7,6 +7,12 @@ import edu.monash.fit2099.engine.Actor;
 import edu.monash.fit2099.engine.GameMap;
 import edu.monash.fit2099.engine.Item;
 
+/**
+ * Returns an eating food action that will allow the actor to comsume the food if it is damaged
+ * 
+ * @author User
+ *
+ */
 public class EatFoodBehaviour implements Behaviour {
 
 	public EatFoodBehaviour() {
@@ -26,6 +32,12 @@ public class EatFoodBehaviour implements Behaviour {
 		return null;
 	}
 	
+	/**
+	 * Get the first Food in the actors inventory
+	 * 
+	 * @param inventory the actor's inventory
+	 * @return the first Food in the actors inventory
+	 */
 	private Food getFood(List<Item> inventory) {
 		for (Item item : inventory) {
 			if (item instanceof Food) {
