9e9cd04087621720ba13cd1599ee4d4bdf82ba4d
Nur Farhanah <nbah0001@student.monash.edu>
2020-05-22 14:13:55 +0800

    Added javadoc for FertilizeAction and FertilizeBehaviour classes

--- src/game/FertilizeAction.java
+++ src/game/FertilizeAction.java
@@ -4,14 +4,29 @@ import edu.monash.fit2099.engine.Action;
 import edu.monash.fit2099.engine.Actor;
 import edu.monash.fit2099.engine.GameMap;
 
+/**
+ * Special Action for fertilizing crops.
+ */
 public class FertilizeAction extends Action {
 	Crop cropToBeFertilized;
-
+	
+	/**
+	 * Constructor.
+	 * 
+	 * @param initCrop the crop to be fertilized
+	 */
 	public FertilizeAction(Crop initCrop) {
 		// TODO Auto-generated constructor stub
 		cropToBeFertilized = initCrop;
 	}
-
+	
+	/**
+	 * Override method to perform the action.
+	 *
+	 * @param actor The actor performing the action.
+	 * @param map The map the actor is on.
+	 * @return a description of what happened that can be displayed to the user.
+	 */
 	@Override
 	public String execute(Actor actor, GameMap map) {
 		// TODO Auto-generated method stub
@@ -19,6 +34,12 @@ public class FertilizeAction extends Action {
 		return menuDescription(actor);
 	}
 
+	/**
+	 * Override method to print a description.
+	 *
+	 * @param actor The actor performing the action.
+	 * @return a description of what happened that can be displayed to the user.
+	 */
 	@Override
 	public String menuDescription(Actor actor) {
 		// TODO Auto-generated method stub
--- src/game/FertilizeBehaviour.java
+++ src/game/FertilizeBehaviour.java
@@ -5,12 +5,26 @@ import edu.monash.fit2099.engine.Actor;
 import edu.monash.fit2099.engine.GameMap;
 import edu.monash.fit2099.engine.Ground;
 
+/**
+ * Returns a FertilizeAction that will make the Actor fertilize a crop.
+ */
 public class FertilizeBehaviour implements Behaviour {
-
+	
+	/**
+	 * Constructor.
+	 * 
+	 */
 	public FertilizeBehaviour() {
 		// TODO Auto-generated constructor stub
 	}
-
+	
+	/**
+	 * Override method to get the action.
+	 *
+	 * @param  actor The actor performing the action.
+	 * @param  map   The map the actor is on.
+	 * @return the FertilizeAction to be performed.
+	 */
 	@Override
 	public Action getAction(Actor actor, GameMap map) {
 		// TODO Auto-generated method stub
