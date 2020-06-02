71091194a52b28b43d7778e763a4af6b10c160fe
Khairenn <klai0011@student.monash.edu>
2020-06-02 14:28:21 +0800

    Added new town map and car to move to town

---
+++ src/game/Car.java
@@ -0,0 +1,27 @@
+package game;
+
+import java.util.List;
+
+import edu.monash.fit2099.engine.Action;
+import edu.monash.fit2099.engine.Actions;
+import edu.monash.fit2099.engine.Item;
+import edu.monash.fit2099.engine.Location;
+
+public class Car extends Item {
+	private Location destination;
+	private String destinationMsg;
+
+	public Car(Location initDestination, String initMessage) {
+		super("car", 'V', false);
+		// TODO Auto-generated constructor stub
+		destination = initDestination;
+		destinationMsg = initMessage;
+	}
+
+	public List<Action> getAllowableActions() {
+		Actions actionsList = new Actions();
+		actionsList.add(new DriveCarAction(destination, destinationMsg));
+		return actionsList.getUnmodifiableActionList();
+	}
+	
+}
