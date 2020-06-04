5bd2b2e73678d63e86843e1a678b92d2a7311af4
Morad <mabo0003@student.monash.edu>
2020-06-04 21:15:44 +0800

    partial class functionality added

--- src/game/MamboMarie.java
+++ src/game/MamboMarie.java
@@ -1,5 +1,37 @@
 package game;
 
-public class MamboMarie {
+import edu.monash.fit2099.engine.Action;
+import edu.monash.fit2099.engine.Actions;
+import edu.monash.fit2099.engine.Display;
+import edu.monash.fit2099.engine.DoNothingAction;
+import edu.monash.fit2099.engine.GameMap;
 
+public class MamboMarie extends Human {
+	
+	private int turnCount;
+	private Behaviour[] behaviours = {
+			new WanderBehaviour(),
+			new ChantingBehaviour()
+	};
+	
+	public MamboMarie(String name) {
+		super(name,'M',100);
+		turnCount = 0;
+		// TODO Auto-generated constructor stub
+	}
+
+
+	@Override
+	public Action playTurn(Actions actions, Action lastAction, GameMap map, Display display) {
+		if (turnCount%10 == 0 && turnCount != 30) {
+			
+		}
+		for (Behaviour behaviour : behaviours) {
+			Action action = behaviour.getAction(this, map);
+			if (action != null) {
+				return action;
+			}
+		}
+		return new DoNothingAction();
+	}
 }
