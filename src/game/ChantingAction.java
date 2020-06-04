08019d1420521d41eb8fcb8eba88a20c1c4ee975
Morad <mabo0003@student.monash.edu>
2020-06-04 21:14:12 +0800

    Updated ChantingAction functionality

--- src/game/ChantingAction.java
+++ src/game/ChantingAction.java
@@ -3,19 +3,32 @@ package game;
 import edu.monash.fit2099.engine.Action;
 import edu.monash.fit2099.engine.Actor;
 import edu.monash.fit2099.engine.GameMap;
+import java.util.Random;
 
 public class ChantingAction extends Action {
+	private static int counter = 0;
 
 	@Override
 	public String execute(Actor actor, GameMap map) {
 		// TODO Auto-generated method stub
-		return null;
+		for (int i = 0; i < 5; i++) {
+			int randXLocation = new Random().nextInt(map.getXRange().max());
+			int randYLocation = new Random().nextInt(map.getYRange().max());
+			while (!map.at(randXLocation, randYLocation).canActorEnter(actor)){
+				randXLocation = new Random().nextInt(map.getXRange().max());
+				randYLocation = new Random().nextInt(map.getYRange().max());
+			}
+			counter += 1;
+			String name = "Vodoo Zombie " + counter;
+			map.at(randXLocation, randYLocation).addActor(new Zombie(name));
+		}
+		return menuDescription(actor);
 	}
 
 	@Override
 	public String menuDescription(Actor actor) {
 		// TODO Auto-generated method stub
-		return null;
+		return actor + " is chanting! There are now 5 new zombies!";
 	}
 
 }
