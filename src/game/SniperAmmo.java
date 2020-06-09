e6d07f733630d0bcd6be45a90b86884b49214bce
Khairenn <klai0011@student.monash.edu>
2020-06-10 03:11:23 +0800

    javadoc for sniperammo

--- src/game/SniperAmmo.java
+++ src/game/SniperAmmo.java
@@ -2,6 +2,10 @@ package game;
 
 public class SniperAmmo extends Ammo {
 
+	/**
+	 * 5 rounds of sniper ammo. Sniper requires ammo to be fired and each shot uses one round.
+	 * 
+	 */
 	public SniperAmmo() {
 		super("sniper ammo", 'r', "sniper", 5);
 		// TODO Auto-generated constructor stub
