65e61b6b6c38aa5b9e81a56868d93b56564aaafa
Nur Farhanah <nbah0001@student.monash.edu>
2020-06-10 06:13:07 +0800

    added javadoc for ExitBehaviour class

--- src/game/ExitBehaviour.java
+++ src/game/ExitBehaviour.java
@@ -5,18 +5,25 @@ import edu.monash.fit2099.engine.Actor;
 import edu.monash.fit2099.engine.GameMap;
 
 /**
- * A class that generates an AttackAction if the current Actor is standing
- * next to an Actor that they can attack.
- * 
- * @author ram
+ * A class that generates an ExitAction if the Actor choose to exit the game.
  *
  */
 public class ExitBehaviour implements Behaviour {
 	
+	/**
+	 * Constructor.
+	 */
 	public ExitBehaviour() {
 		// TODO Auto-generated constructor stub
 	}
 
+	/**
+	 * Returns an DriveCarAction that allows Actor to drive to another location.
+	 * 
+	 * @param actor The actor driving the car
+	 * @param map The map the actor is on
+	 * @return the ExitAction that allows Actor to to exit the game
+	 */
 	@Override
 	public Action getAction(Actor actor, GameMap map) {
 		return new ExitAction();
