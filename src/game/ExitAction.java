4c7f50a8c711848f41f87caef3260314ee1188ed
Nur Farhanah <nbah0001@student.monash.edu>
2020-06-10 06:12:40 +0800

    added javadoc for ExitAction class

--- src/game/ExitAction.java
+++ src/game/ExitAction.java
@@ -4,11 +4,25 @@ import edu.monash.fit2099.engine.Action;
 import edu.monash.fit2099.engine.Actor;
 import edu.monash.fit2099.engine.GameMap;
 
+/**
+ * Special action for exiting the game
+ *
+ */
 public class ExitAction extends Action {
 
+	/**
+	 * Constructor
+	 */
 	public ExitAction() {
 	}
 
+	/**
+	 * Performs the exit game action
+	 * 
+	 * @param actor The actor exiting the game.
+	 * @param map The map the actor is on.
+	 * @return the menu description of the action.
+	 */
 	@Override
 	public String execute(Actor actor, GameMap map) {
 		// TODO Auto-generated method stub
