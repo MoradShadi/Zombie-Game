e626246dba30a5b69578a7f33c9f263a2a73cdd1
Nur Farhanah <nbah0001@student.monash.edu>
2020-06-10 06:11:39 +0800

    added javadoc for Car class

--- src/game/Car.java
+++ src/game/Car.java
@@ -8,10 +8,19 @@ import edu.monash.fit2099.engine.Item;
 import edu.monash.fit2099.engine.Location;
 import edu.monash.fit2099.engine.Weapon;
 
+/**
+ * A class that represents a car.
+ */
 public class Car extends Item {
 	private Location destination;
 	private String destinationMsg;
 
+	/**
+	 * Constructor
+	 * 
+	 * @param initDestination The destination to drive the car to
+	 * @param initMessage The message which indicates to destination
+	 */
 	public Car(Location initDestination, String initMessage) {
 		super("car", 'V', false);
 		// TODO Auto-generated constructor stub
@@ -19,16 +28,25 @@ public class Car extends Item {
 		destinationMsg = initMessage;
 	}
 
-	// Getter for destination
+	/**
+	 * Getter for getDestination
+	 */
 	public Location getDestination() {
 		return destination;
 	}
 	
-	//Getter for destinationMsg
+	/**
+	 * Getter for getDestinationMsg
+	 */
 	public String getDestinationMsg() {
 		return destinationMsg;
 	}
 
+	/**
+	 * Gets a list of unmodifiable actions
+	 * 
+	 * @return a list of unmodifiable actions
+	 */
 	public List<Action> getAllowableActions() {
 		Actions actionsList = new Actions();
 		actionsList.add(new DriveCarAction(destination, destinationMsg));
