cd6da1542712c5d3bc5ed2ae35f4c9651f0ced14
Nur Farhanah <nbah0001@student.monash.edu>
2020-06-10 06:12:06 +0800

    added javadoc for DriveCarAction class

--- src/game/DriveCarAction.java
+++ src/game/DriveCarAction.java
@@ -6,17 +6,34 @@ import edu.monash.fit2099.engine.GameMap;
 import edu.monash.fit2099.engine.Location;
 import edu.monash.fit2099.engine.MoveActorAction;
 
+/**
+ * Special action for driving a car
+ *
+ */
 public class DriveCarAction extends Action {
 	
 	private Location destination;
 	private String displayMsg;
 
+	/**
+	 * Constructor
+	 * 
+	 * @param initDestination the destination to drive to
+	 * @param initMessage the message which indicates the destination
+	 */
 	public DriveCarAction(Location initDestination, String initMessage) {
 		// TODO Auto-generated constructor stub
 		this.destination = initDestination;
 		displayMsg = initMessage;
 	}
 
+	/**
+	 * Performs the driving action
+	 * 
+	 * @param actor The actor that is driving.
+	 * @param map The map the actor is on.
+	 * @return the execution of moving the actor to the destination
+	 */
 	@Override
 	public String execute(Actor actor, GameMap map) {
 		// TODO Auto-generated method stub
