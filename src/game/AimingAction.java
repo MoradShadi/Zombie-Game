aae7a699b1499f49a8fbfdb826d043f5f06557a8
Khairenn <klai0011@student.monash.edu>
2020-06-12 03:53:03 +0800

    Added javadoc for aiming action

--- src/game/AimingAction.java
+++ src/game/AimingAction.java
@@ -4,16 +4,31 @@ import edu.monash.fit2099.engine.Action;
 import edu.monash.fit2099.engine.Actor;
 import edu.monash.fit2099.engine.GameMap;
 
+/**
+ * An action that allows the Actor to aim a gun for increased accuracy and damage
+ * 
+ * @author User
+ *
+ */
 public class AimingAction extends Action {
 	Actor target;
 	Sniper sniper;
 
+	/**
+	 * Constructor
+	 * 
+	 * @param initSniper sniper that is used for aiming
+	 * @param initTarget the target that is aimed at
+	 */
 	public AimingAction(Sniper initSniper, Actor initTarget) {
 		// TODO Auto-generated constructor stub
 		this.target = initTarget;
 		this.sniper = initSniper;
 	}
 
+	/**
+	 * Perform the aiming action and adjust the accuracy and damage accordingly by calling the aimSniper method
+	 */
 	@Override
 	public String execute(Actor actor, GameMap map) {
 		// TODO Auto-generated method stub
